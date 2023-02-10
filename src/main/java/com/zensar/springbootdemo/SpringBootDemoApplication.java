79675ef501153d13342782e947d795ff4e2ea1ef
sharadrajore <42150372+sharadrajore@users.noreply.github.com>
2023-02-10 16:29:34 +0530

    Removed unused println statement

--- src/main/java/com/zensar/springbootdemo/SpringBootDemoApplication.java
+++ src/main/java/com/zensar/springbootdemo/SpringBootDemoApplication.java
@@ -14,7 +14,6 @@ public class SpringBootDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String... args) {
 		
-		System.out.println("Hi");
 		
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 		
